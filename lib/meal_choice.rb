67257b7019d9a943799b190173010a2316fddd92
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-08 06:15:56 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,3 @@
-def meal_choice("broccoli", "macaroni")
-put "A plate of meat with #{broccoli} and #{macaroni}."
+def meal_choice("broccoli", "macaroni", "meat")
+put "A plate of #{meat} with #{broccoli} and #{macaroni}."
 end
