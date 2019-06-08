2089cecc62d9d09c0c5d6fe51cd3c895bc8092d1
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-08 06:45:49 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,5 @@
-# Your code here
+describe '#meal_choice' do
+  it 'should default to meat for the protein' do
+def meal_choice(protein, broccoli, macaroni)
+  puts "A plate of #{protein}. with #{broccoli} and #{macaroni}."
+end
