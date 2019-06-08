38977e3c6c154e0d9d6c73b22154b68b88443be9
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-08 06:45:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,5 @@
+describe '#meal_choice' do
+  it 'should default to meat for the protein' do
 def meal_choice(protein, broccoli, macaroni)
   puts "A plate of #{protein}. with #{broccoli} and #{macaroni}."
 end
